c668738abf9724138f0a7bc55fb3a8fd5e447cbd
cristianebrolese <cristianebroleseb15@gmail.com>
2018-08-15 08:22:05 -0300

    Criado objeto Produto e imprimindo uma mensagem com o getNome e o definePrecoDeVenda.

--- sistemavenda1/src/br/edu/ifcvideira/sistemavenda1/Main.java
+++ sistemavenda1/src/br/edu/ifcvideira/sistemavenda1/Main.java
@@ -1,5 +1,11 @@
 package br.edu.ifcvideira.sistemavenda1;
 
 public class Main {
+	
+	public static void main(String[] args){
+		Produto p1 = new Produto("Caneta", 1.00, 10);
+		System.out.println("Nome do Produto: " + p1.getNome());
+		p1.definePrecoDeVenda(30);
+	}
 
 }
