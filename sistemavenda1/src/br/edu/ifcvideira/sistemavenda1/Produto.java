fa5ac487120eaf740153fbcd4c0771fefde77f2c
cristianebrolese <cristianebroleseb15@gmail.com>
2018-08-15 08:04:58 -0300

    Adicionado método public Produto

--- sistemavenda1/src/br/edu/ifcvideira/sistemavenda1/Produto.java
+++ sistemavenda1/src/br/edu/ifcvideira/sistemavenda1/Produto.java
@@ -5,5 +5,11 @@ public class Produto {
 	private String nome;
 	private double precoDeCompra;
 	private int quantidadeDeEstoque;
+	
+	public Produto(String nome, double precoDeCompra, int quantidadeDeEstoque){
+		this.nome = nome;
+		this.precoDeCompra = precoDeCompra;
+		this.quantidadeDeEstoque = quantidadeDeEstoque;
+	}
 
 }
