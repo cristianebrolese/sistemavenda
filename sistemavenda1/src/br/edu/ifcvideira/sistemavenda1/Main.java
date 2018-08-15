f3b46496f825d5a4f84a1d8679438e1e6134bb96
cristianebrolese <cristianebroleseb15@gmail.com>
2018-08-15 08:27:51 -0300

    Imprimindo valor de compra com o getPrecoDeCompra.

--- sistemavenda1/src/br/edu/ifcvideira/sistemavenda1/Main.java
+++ sistemavenda1/src/br/edu/ifcvideira/sistemavenda1/Main.java
@@ -4,8 +4,13 @@ public class Main {
 	
 	public static void main(String[] args){
 		Produto p1 = new Produto("Caneta", 1.00, 10);
+		
 		System.out.println("Nome do Produto: " + p1.getNome());
 		p1.definePrecoDeVenda(30);
+		
+		System.out.println("Valor de Compra: " + p1.getPrecoDeCompra());
+		p1.vende(8);
+		
 	}
 
 }
